69f9ae36f55fa8d4be9d4694285f14822d4f2d28
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-07-22 16:00:29 +0200

    tested three test suites

---
+++ src/mock-data.js
@@ -0,0 +1,11 @@
+const mockData = [
+  {
+     "kind": "calendar#events",
+     "etag": "\"p3308h454qfgum0g\""
+  },
+  {"kind": "calendar#event",
+   "etag": "\"3181161784712000\""
+}
+]
+// mockData =JSON.parse(JSON.stringify(mockData));
+export {mockData};
